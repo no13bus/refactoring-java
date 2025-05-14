069509bd6fd87264c193cc8f43bd46d32ef718f6
qihui <qihui.jia@mods.se>
2025-05-14 22:18:44 +0200

    perf: use StringBuilder in statement method to optimize string concatenation

--- src/main/java/movierental/RentalInfo.java
+++ src/main/java/movierental/RentalInfo.java
@@ -5,17 +5,17 @@ public class RentalInfo {
     public String statement(Customer customer) {
         double totalAmount = 0;
         int frequentEnterPoints = 0;
-        String result = "Rental Record for " + customer.getName() + "\n";
+        StringBuilder result = new StringBuilder("Rental Record for " + customer.getName() + "\n");
         for (MovieRental r : customer.getRentals()) {
             double thisAmount = r.calculateAmount();
             // add frequent bonus points
             frequentEnterPoints += r.calculateFrequentEnterPoints();
-            result += "\t" + r.getMovie().getTitle() + "\t" + thisAmount + "\n";
+            result.append("\t").append(r.getMovie().getTitle()).append("\t").append(thisAmount).append("\n");
             totalAmount += thisAmount;
         }
         // add footer lines
-        result += "Amount owed is " + totalAmount + "\n";
-        result += "You earned " + frequentEnterPoints + " frequent points\n";
-        return result;
+        result.append("Amount owed is ").append(totalAmount).append("\n");
+        result.append("You earned ").append(frequentEnterPoints).append(" frequent points\n");
+        return result.toString();
     }
 }
